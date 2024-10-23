9e12524966e52061aa0bf19001adb73114257eea
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-10-23 20:23:35 +0100

    feat: rss feed

---
+++ src/app/routes/rss[.xml].tsx
@@ -0,0 +1,40 @@
+import { LoaderFunction } from "@remix-run/cloudflare";
+import { Feed } from "feed";
+import { getPosts } from "src/atproto/getPosts";
+
+export const loader: LoaderFunction = async ({ context }) => {
+  const posts = await getPosts(context, undefined);
+
+  const postsFiltered = posts.filter(p => !p.content?.startsWith('NOT_LIVE'));
+
+  const postsShortened = postsFiltered.map(p => {
+    if (p.content?.length! > 200) {
+      p.content = p.content?.slice(0, 200).trimEnd() + '...';
+    }
+    return p;
+  });
+
+  const blogLink = 'https://hayden.moe/posts';
+  const feed = new Feed({
+    id: blogLink,
+    title: 'hayden@web ~/posts',
+    description: '',
+    link: blogLink,
+    language: 'en',
+    updated: postsShortened.length > 0 ? new Date(postsShortened[0].createdAt) : new Date(),
+    generator: 'https://github.com/jpmonette/feed',
+    copyright: '© Hayden Young',
+  });
+
+  postsShortened.forEach(post => feed.addItem({
+    id: `${blogLink}/${post.cid}/`,
+    title: post.title,
+    link: `${blogLink}/${post.cid}/`,
+    date: new Date(post.createdAt),
+    description: post.content,
+  }));
+
+  return new Response(feed.rss2(), {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+};
