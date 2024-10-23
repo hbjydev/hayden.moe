a76c1d3f2f5821d8374af1faa037e3126c07d54e
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-10-23 01:27:45 +0100

    feat: meta for posts

--- src/app/routes/posts.$rkey.tsx
+++ src/app/routes/posts.$rkey.tsx
@@ -1,9 +1,40 @@
-import { json, LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { json, LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
 import { getPost } from 'src/atproto/getPost';
 import { WhtwndBlogEntryView } from 'src/types';
 import { FormattedDate } from '../components/formatted-date';
 import Markdown from 'react-markdown';
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (data) {
+    const desc = data.post.content?.slice(0, 100).trimEnd();
+    const url = `https://hayden.moe/${data.post.rkey}`;
+
+    return [
+      // HTML Meta
+      { title: data.post.title },
+      { name: 'description', content: desc },
+
+      // OG Meta
+      { property: 'og:url', content: url },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: data.post.title },
+      { property: 'og:description', content: desc },
+
+      // Twitter Meta
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:domain', content: 'hayden.moe' },
+      { property: 'twitter:url', content: url },
+      { name: 'twitter:title', content: data.post.title },
+      { name: 'twitter:description', content: desc },
+    ];
+  } else {
+    return [
+      { title: 'Not found' },
+      { name: 'description', content: "This post doesn't exist!" },
+    ];
+  }
+}
 
 export const loader = async ({ params, context }: LoaderFunctionArgs) => {
   const { rkey } = params;
