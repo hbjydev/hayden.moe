5454ad7a672f51746a444022a3a6cb6ae77c1b49
Hayden Young <hayden.young@zoodigital.com>
2025-07-18 12:17:13 +0100

    feat: declare git hash as a global via vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import childProcess from "child_process";
 
 import tailwindcss from '@tailwindcss/vite';
+
+// obtain Git commit hash
+const hash = childProcess
+  .execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();
 
 export default defineConfig({
   site: 'https://hayden.moe',
@@ -14,6 +21,9 @@ export default defineConfig({
   }),
   vite: {
     plugins: [tailwindcss()],
+    define: {
+      __GIT_HASH__: JSON.stringify(hash),
+    },
     build: {
       rollupOptions: {
         external: ['shiki/onig.wasm'],
--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="astro/client" />
 
+declare var __GIT_HASH__: string;
+
 // use a default runtime configuration (advanced mode).
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
