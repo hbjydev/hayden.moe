f813ab5b199c993dd5de7db26a2b684e619a5f6e
Hayden Young <hayden@hayden.moe>
2025-11-13 05:24:48 +0000

    feat: moving to leaflet

--- src/atproto/agent.ts
+++
@@ -1,5 +0,0 @@
-import { AtpAgent } from '@atproto/api';
-
-export const atpAgent = (locals: App.Locals) => new AtpAgent({
-  service: locals.runtime.env.ATP_SERVICE,
-});
--- src/atproto/uriToRkey.ts
+++
@@ -1,8 +0,0 @@
-export const uriToRkey = (uri: string): string => {
-  const rkey = uri.split('/').pop()
-  if (!rkey) {
-    throw new Error('Failed to get rkey from uri.')
-  } else {
-    return rkey
-  }
-}
